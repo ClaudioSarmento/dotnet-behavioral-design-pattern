2076ac6cb275abd6e51237c78f19c49b97408c84
ClaudioSarmento <claudio-s-i@hotmail.com>
2026-02-24 21:21:21 -0300

    Chain of Responsability - Evitar o acoplamento do remetente de uma solicitação ao seu receptor, ao dar a mais de um objeto a oportunidade de tratar a solicitação. Encadear os objetos receptores, passando a solicitação ao longo da cadeia até que um objeto a trate.

---
+++ PadroesDeProjetoComportamental/ChainofResponsability/Handler.cs
@@ -0,0 +1,13 @@
+﻿namespace ChainofResponsability;
+
+public abstract class Handler
+{
+    protected Handler successor;
+
+    public void SetSuccessor(Handler successor)
+    {
+        this.successor = successor;
+    }
+
+    public abstract void HandleRequest(int request);
+}
